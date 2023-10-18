fac355c6ce54db825e0695c2cde91ddbfc585f6b
Topia99 <1242911982z@gmail.com>
2023-10-17 22:03:07 -0700

    filter queryset display with ?collection_id=?

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
 
