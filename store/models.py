a056cf6670497d685e8bd237a68413c5d6bf0db0
Topia99 <1242911982z@gmail.com>
2023-10-19 14:10:45 -0700

    implement features (Add CartItem, Update CartItem, Delete CartItem) to shopping cart

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from uuid import uuid4
 
 
@@ -111,7 +112,9 @@ class CartItem(models.Model):
     
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField() 
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    ) 
     
     class Meta:
         unique_together = [['cart', 'product']]
--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyM
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
 from .models import Product, Collection, OrderItem, Review, Cart, CartItem
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 from .filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
@@ -56,7 +56,17 @@ class CartViewSet(CreateModelMixin, GenericViewSet, RetrieveModelMixin, DestroyM
     serializer_class = CartSerializer
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
     
     def get_queryset(self):
         return CartItem.objects. \
